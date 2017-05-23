61f4b10240db896fe33b3cfdf710e0e15aa4d949
Colin Weber <admin@terratex.eu>
2017-05-23 14:56:39 +0200

    forbid external connections

--- resources/LocalTelnetAdmin/LocalTelnetAdmin.cs
+++ resources/LocalTelnetAdmin/LocalTelnetAdmin.cs
@@ -33,7 +33,7 @@ namespace LocalTelnetAdmin
                 // Enter the listening loop. 
                 while (true)
                 {
-                    Console.Write("Telnet Adminconnection can be recieved on: " + localAddr.ToString() + ":" + port);
+                    Console.WriteLine("Telnet Open Server Connetion on: " + localAddr.ToString() + ":" + port);
 
                     // Perform a blocking call to accept requests. 
                     // You could also user server.AcceptSocket() here.
@@ -42,31 +42,39 @@ namespace LocalTelnetAdmin
                     String clientIp = ((IPEndPoint) client.Client.RemoteEndPoint).Address.ToString();
                     Console.WriteLine("Recieved Adminconnection from " + clientIp);
 
-                    data = null;
-
-                    // Get a stream object for reading and writing
-                    NetworkStream stream = client.GetStream();
-
-                    int i;
-
-                    // Loop to receive all the data sent by the client. 
-                    while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+                    if (!clientIp.Equals("127.0.0.1"))
                     {
-                        // Translate data bytes to a ASCII string.
-                        data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
-                        Console.WriteLine("Received: {0}", data);
+                        Console.WriteLine("Connection only allowed from LocalHost");
+                        client.Close();
+                    }
+                    else
+                    {
+                        data = null;
+
+                        // Get a stream object for reading and writing
+                        NetworkStream stream = client.GetStream();
+
+                        int i;
+
+                        // Loop to receive all the data sent by the client. 
+                        while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+                        {
+                            // Translate data bytes to a ASCII string.
+                            data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
+                            Console.WriteLine("Received: {0}", data);
 
 
-                        
 
-                        // Process the data sent by the client.
-//                        data = data.ToUpper();
 
-//                        byte[] msg = System.Text.Encoding.ASCII.GetBytes(data);
+                            // Process the data sent by the client.
+                            //                        data = data.ToUpper();
 
-//                         Send back a response.
-//                        stream.Write(msg, 0, msg.Length);
-//                        Console.WriteLine("Sent: {0}", data);
+                            //                        byte[] msg = System.Text.Encoding.ASCII.GetBytes(data);
+
+                            //                         Send back a response.
+                            //                        stream.Write(msg, 0, msg.Length);
+                            //                        Console.WriteLine("Sent: {0}", data);
+                        }
                     }
 
                     // Shutdown and end connection
