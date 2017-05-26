a4c69d0b4756097883317bda4de03b80413cb9f3
Geramy92 <admin@terratex.eu>
2017-05-26 19:29:39 +0200

    Add Config reading and change meta to work with compiled

---
+++ resources/TerraTex-RL-RPG/Lib/Data/Database.cs
@@ -0,0 +1,18 @@
+﻿using System.Xml;
+using MySql.Data.MySqlClient;
+
+namespace TerraTex_RL_RPG.Lib.Data
+{
+    public class Database
+    {
+        private MySqlConnection _conn;
+
+        public Database()
+        {
+            XmlDocument databaseConfig = TTRPG.Configs.GetConfig("database");
+
+            
+        }
+        
+    }
+}
--- resources/TerraTex-RL-RPG/TerraTex_RL_RPG.cs
+++ resources/TerraTex-RL-RPG/TerraTex_RL_RPG.cs
@@ -1,19 +1,29 @@
-﻿using GrandTheftMultiplayer.Server.API;
-using TerraTex_RL_RPG.Lib.Data.Database;
+﻿using System;
+using System.IO;
+using System.Reflection;
+using GrandTheftMultiplayer.Server.API;
+using TerraTex_RL_RPG.Lib.Data;
 
 namespace TerraTex_RL_RPG
 {
     public class TTRPG : Script
     {
-        static public Database mysql = new Database();
+        static public Database Mysql;
+        static public Configs Configs;
+        static public API Api;
 
         public TTRPG()
         {
+            TTRPG.Api = API;
             API.onResourceStart += ttStartUp;
         }
 
         public void ttStartUp()
         {
+            TTRPG.Configs = new Configs();
+            TTRPG.Mysql = new Database();
+
+
             API.consoleOutput("Starting TerraTex_RL_RPG Gamemode");
         }
     }
