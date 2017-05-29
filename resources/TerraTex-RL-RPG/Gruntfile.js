41573207a46d91c5762e04279d5c6e74cadfd683
Geramy92 <admin@terratex.eu>
2017-05-29 12:15:03 +0200

    add basic stylesheets and npm files

---
+++ resources/TerraTex-RL-RPG/Gruntfile.js
@@ -0,0 +1,15 @@
+﻿module.exports = function (grunt) {
+
+    grunt.initConfig({
+        sass: {                            
+            dist: {
+                files: {                         
+                    'UI/general/Styles/bootstrap/bootstrap.css': 'UI/general/Styles/bootstrap/bootstrap.scss'    
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-sass');
+    grunt.registerTask('default', ['sass']);
+};
