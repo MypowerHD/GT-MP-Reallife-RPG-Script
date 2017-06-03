dc9d695b05ef987a7063fa646028baa2bdc3cef5
Geramy92 <admin@terratex.eu>
2017-06-03 12:32:07 +0200

    Apply Data to Player

--- resources/TerraTex-RL-RPG/Lib/User/StartUp/PlayerLogin.cs
+++ resources/TerraTex-RL-RPG/Lib/User/StartUp/PlayerLogin.cs
@@ -49,7 +49,7 @@ namespace TerraTex_RL_RPG.Lib.User.StartUp
                     updateUserEntryCommand.ExecuteNonQuery();
 
                     EnsureAllDatabaseTableEntries((Int32)result.Rows[0]["ID"]);
-                    StartLoginProcess(player, result);
+                    StartLoginProcess(player, result.Rows[0]);
                 }
                 else
                 {
@@ -59,7 +59,7 @@ namespace TerraTex_RL_RPG.Lib.User.StartUp
             }
         }
 
-        public void EnsureAllDatabaseTableEntries(int dbUserId)
+        private void EnsureAllDatabaseTableEntries(int dbUserId)
         {
             string[] tables = new string[]
             {
@@ -83,21 +83,51 @@ namespace TerraTex_RL_RPG.Lib.User.StartUp
             }
         }
 
-        public void StartLoginProcess(Client player, DataTable dbUser)
+        private void StartLoginProcess(Client player, DataRow dbUser)
         {
             // read all data here and store it to player before starting spawn
-            int userId = (int) dbUser.Rows[0]["ID"];
+            int userId = (int) dbUser["ID"];
+            ApplyTableToPlayerUser(player, dbUser);
+
             DataRow userData = GetDataFromUserTable("user_data", userId);
+            ApplyTableToPlayerUserData(player, userData);
+
             DataRow userInventory = GetDataFromUserTable("user_inventory", userId);
-            
+            ApplyTableToPlayerUserInventory(player, userInventory);
 
         }
 
-        public DataRow GetDataFromUserTable(string table, int userID)
+        private void ApplyTableToPlayerUser(Client player, DataRow data)
+        {
+            player.setSyncedData("ID", (int)data["ID"]);
+            player.setSyncedData("UUID", (string)data["UUID"]);
+            player.setSyncedData("Nickname", (string)data["Nickname"]);
+            player.setSyncedData("Forename", (string)data["Forename"]);
+            player.setSyncedData("Lastname", (string)data["Lastname"]);
+            player.setSyncedData("Gender", (string)data["Gender"]);
+            player.setSyncedData("Birthday", (string)data["Birthday"]);
+            player.setSyncedData("History", (string)data["History"]);
+            player.setSyncedData("Admin", (int)data["Admin"]);
+            player.setSyncedData("Dev", (int)data["Dev"]);
+        }
+
+        private void ApplyTableToPlayerUserData(Client player, DataRow data)
+        {
+            player.setSyncedData("PlayTime", (int) data["PlayTime"]);
+        }
+
+        private void ApplyTableToPlayerUserInventory(Client player, DataRow data)
+        {
+            player.setSyncedData("Money", (int) data["Money"]);
+            player.setSyncedData("BankAccount", (int) data["BankAccount"]);
+            player.setSyncedData("Phone", (int) data["Phone"]);
+        }
+
+        private DataRow GetDataFromUserTable(string table, int userId)
         {
             MySqlCommand getUserDataCommand = TTRPG.Mysql.Conn.CreateCommand();
             getUserDataCommand.CommandText = "SELECT * FROM " + table + " WHERE UserID = @id";
-            getUserDataCommand.Parameters.AddWithValue("id", userID);
+            getUserDataCommand.Parameters.AddWithValue("id", userId);
             DataTable result = new DataTable();
             result.Load(getUserDataCommand.ExecuteReader());
             return result.Rows[0];
