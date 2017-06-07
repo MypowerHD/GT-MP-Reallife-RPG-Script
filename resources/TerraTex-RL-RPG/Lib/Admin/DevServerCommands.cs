46dbd32def6e72ffba167a7114157219ea18969f
Geramy92 <admin@terratex.eu>
2017-06-07 21:37:05 +0200

    add create vehicle

--- resources/TerraTex-RL-RPG/Lib/Admin/DevServerCommands.cs
+++ resources/TerraTex-RL-RPG/Lib/Admin/DevServerCommands.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -7,6 +8,7 @@ using System.Threading.Tasks;
 using GrandTheftMultiplayer.Server.API;
 using GrandTheftMultiplayer.Server.Elements;
 using GrandTheftMultiplayer.Server.Managers;
+using GrandTheftMultiplayer.Shared;
 using GrandTheftMultiplayer.Shared.Math;
 
 namespace TerraTex_RL_RPG.Lib.Admin
@@ -32,6 +34,20 @@ namespace TerraTex_RL_RPG.Lib.Admin
                 string appendText = position.X + ", " + position.Y + ", " + position.Z + " // " + info + Environment.NewLine;
                 File.AppendAllText(path, appendText);
                 player.sendNotification("Dev-System", "Saved Position: X: " + position.X + "; Y: " + position.Y + "; Z: " + position.Z);
+            }
+        }
+
+        [Command("veh", Group = "dev", SensitiveInfo = true)]
+        public void VehCommand(Client player, string vehicleModelName)
+        {
+            if (DevServer.CheckDevCommandAccess(player) || AdminChecks.CheckAdminLvl(player, 3))
+            {
+                Vector3 position = player.position.Add(new Vector3(2,2,2));
+                Vector3 rotation = player.rotation;
+
+                VehicleHash myVehicle = API.vehicleNameToModel(vehicleModelName);
+                Random rnd = new Random();
+                API.createVehicle(myVehicle, position, rotation, rnd.Next(0, 159), rnd.Next(0, 159));
             }
         }
     }
