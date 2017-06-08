376a70979bb99cb7a98df599518a7de6c2d0d200
Geramy92 <admin@terratex.eu>
2017-06-08 13:17:44 +0200

    Fix sonar issues

--- resources/TerraTex-RL-RPG/UI/custom/clipboard.js
+++ resources/TerraTex-RL-RPG/UI/custom/clipboard.js
@@ -2,11 +2,13 @@
     if (document.queryCommandSupported && document.queryCommandSupported("copy")) {
         var textarea = document.createElement("textarea");
         textarea.textContent = text;
-        textarea.style.position = "fixed"; // Prevent scrolling to bottom of page in MS Edge.
+        // Prevent scrolling to bottom of page in MS Edge.
+        textarea.style.position = "fixed";
         document.body.appendChild(textarea);
         textarea.select();
         try {
-            return document.execCommand("copy"); // Security exception may be thrown by some browsers.
+            // Security exception may be thrown by some browsers.
+            return document.execCommand("copy");
         } catch (ex) {
             console.warn("Copy to clipboard failed.", ex);
             return false;
