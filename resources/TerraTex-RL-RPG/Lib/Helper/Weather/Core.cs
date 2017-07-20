d0e1a51416ddde1302e576d16ec6f156499a42ca
Timo Pollmeier <MypowerHD@cryz-games.de>
2017-07-20 02:21:25 +0200

    Umstruckturierung des Wettersystems vorbereitung für Neuerungen
    Signed-off-by: Timo Pollmeier <MypowerHD@cryz-games.de>

---
+++ resources/TerraTex-RL-RPG/Lib/Helper/Weather/Core.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TerraTex_RL_RPG.Lib.Helper;
+
+namespace TerraTex_RL_RPG.Lib.Helper.Weather
+{
+    public class Core
+    {
+        public int weatherID = 0;
+        public string weatherName = "Not Set ATM";
+
+        public void Boot()
+        {
+            getNewWeatherID();
+            getWeatherName();
+        }
+
+        public void getNewWeatherID()
+        {
+            var _loadedDie = new LoadedDie(new int[] {
+                    155, //Extra Sunny
+                    155, //Clear
+                    155, //Clouds
+                    45, //Smog
+                    45, //Foggy
+                    45, //Overcast
+                    45, //Rain
+                    45, //Thunder
+                    45, //Light rain
+                    45, //Smoggy light rain
+                    45, //Very light Snow
+                    45, //Windy light Snow
+                    45, //Light snow
+                    45 //Unknown (No Effect)*/
+                });
+
+            weatherID = _loadedDie.NextValue();
+        }
+
+        public void getWeatherName()
+        {
+            switch (weatherID)
+            {
+                case 0: weatherName = "Extra Sunny"; break;
+                case 1: weatherName = "Clear"; break;
+                case 2: weatherName = "Clouds"; break;
+                case 3: weatherName = "Smog"; break;
+                case 4: weatherName = "Foggy"; break;
+                case 5: weatherName = "Overcast"; break;
+                case 6: weatherName = "Rain"; break;
+                case 7: weatherName = "Thunder"; break;
+                case 8: weatherName = "Light rain"; break;
+                case 9: weatherName = "Smoggy light rain"; break;
+                case 10: weatherName = "Very light Snow"; break;
+                case 11: weatherName = "Windy light Snow"; break;
+                case 12: weatherName = "Light snow"; break;
+                case 13: weatherName = "Unknown (No Effect)"; break;
+            }
+        }
+    }
+}
--- resources/TerraTex-RL-RPG/Lib/Threads/UpdateWeather.cs
+++ resources/TerraTex-RL-RPG/Lib/Threads/UpdateWeather.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using TerraTex_RL_RPG.Lib.Helper;
+using TerraTex_RL_RPG.Lib.Helper.Weather;
 using System.Threading;
 
 namespace TerraTex_RL_RPG.Lib.Threads
@@ -16,46 +16,12 @@ namespace TerraTex_RL_RPG.Lib.Threads
         {
             TTRPG.Api.consoleOutput("Started DynamicWeather Thread");
             while (!_interuped)
-            {
-                var _loadedDie = new LoadedDie(new int[] {
-                    155, //Extra Sunny
-                    155, //Clear
-                    155, //Clouds
-                    45, //Smog
-                    45, //Foggy
-                    45, //Overcast
-                    45, //Rain
-                    45, //Thunder
-                    45, //Light rain
-                    45, //Smoggy light rain
-                    45, //Very light Snow
-                    45, //Windy light Snow
-                    45, //Light snow
-                    45 //Unknown (No Effect)
-                });
-
-                int _weatherID = _loadedDie.NextValue();
-                string weather = "";
-                switch (_weatherID)
-                {
-                    case 0: weather = "Extra Sunny"; break;
-                    case 1: weather = "Clear"; break;
-                    case 2: weather = "Clouds"; break;
-                    case 3: weather = "Smog"; break;
-                    case 4: weather = "Foggy"; break;
-                    case 5: weather = "Overcast"; break;
-                    case 6: weather = "Rain"; break;
-                    case 7: weather = "Thunder"; break;
-                    case 8: weather = "Light rain"; break;
-                    case 9: weather = "Smoggy light rain"; break;
-                    case 10: weather = "Very light Snow"; break;
-                    case 11: weather = "Windy light Snow"; break;
-                    case 12: weather = "Light snow"; break;
-                    case 13: weather = "Unknown (No Effect)"; break;
-                }
-
-                TTRPG.Api.consoleOutput("[DynamicWeather] The Weather has changed to " + weather);
-                TTRPG.Api.setWeather(_weatherID);
+            {          
+                var _core = new Core();
+                _core.Boot();
+                
+                TTRPG.Api.consoleOutput("[DynamicWeather] The Weather has changed to " + _core.weatherName);
+                TTRPG.Api.setWeather(_core.weatherID);
 
                 Thread.Sleep(1200000);
             }
